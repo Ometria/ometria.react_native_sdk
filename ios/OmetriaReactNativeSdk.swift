633d300753fe37b01d7e2dd6014c45b9dafd5fe4
Hetmann Wilhelm Iohan <contact@react-ui-kit.com>
2020-11-17 11:48:37 +0200

    WIP: expose methods from RN native modules, added Swift methods

--- ios/OmetriaReactNativeSdk.swift
+++ ios/OmetriaReactNativeSdk.swift
@@ -2,16 +2,59 @@ import Ometria
 
 @objc(OmetriaReactNativeSdk)
 class OmetriaReactNativeSdk: NSObject {
-
-    @objc(multiply:withB:withResolver:withRejecter:)
-    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a*b)
-    }
-
     @objc(initializeWithApiToken:withResolver:withRejecter:)
     func initialize(apiToken: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
         let ometriaInit = Ometria.initialize(apiToken: apiToken)
         resolve(ometriaInit)
+    }
+
+    @objc(trackProfileIdentifiedEventByCustomerID:withResolver:withRejecter:)
+    func trackProfileIdentifiedEventByCustomerID(customerId: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve()
+    }
+    @objc(trackProfileIdentifiedEventByEmail:withResolver:withRejecter:)
+    func trackProfileIdentifiedEventByEmail(email: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve()
+    }
+    @objc(trackProductViewedEvent:withResolver:withRejecter:)
+    func trackProductViewedEvent(productId: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve()
+    }
+    @objc(trackProductCategoryViewedEvent:withResolver:withRejecter:)
+    func trackProductCategoryViewedEvent(category: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve()
+    }
+    @objc(trackWishlistAddedToEvent:withResolver:withRejecter:)
+    func trackWishlistAddedToEvent(productId: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve()
+    }
+    @objc(trackWishlistRemovedFromEvent:withResolver:withRejecter:)
+    func trackWishlistRemovedFromEvent(productId: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve()
+    }
+    @objc(trackBasketViewedEvent:withResolver:withRejecter:)
+    func trackBasketViewedEvent(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve()
+    }
+    @objc(trackBasketUpdatedEvent:withResolver:withRejecter:)
+    func trackBasketUpdatedEvent(basket: OmetriaBasket, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve()
+    }
+    @objc(trackOrderCompletedEvent:withResolver:withRejecter:)
+    func trackOrderCompletedEvent(orderId: String, basket: OmetriaBasket, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve()
+    }
+    @objc(trackDeepLinkOpenedEvent:withResolver:withRejecter:)
+    func trackDeepLinkOpenedEvent(link: String, screenName: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve()
+    }
+    @objc(trackScreenViewedEvent:withResolver:withRejecter:)
+    func trackScreenViewedEvent(screenName: String, additionalInfo: [String: Any], resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve()
+    }
+    @objc(trackCustomEvent:withResolver:withRejecter:)
+    func trackCustomEvent(customEventType: String, additionalInfo: [String: Any], resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve()
     }
 
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,35 @@
 import { NativeModules } from 'react-native';
 
+type OmetriaBasketItem = {
+  productId: string;
+  sku: string;
+  quantity: number;
+  price: number;
+}
+
+type OmetriaBasket = {
+  currency: string;
+  totalPrice: number;
+  item: [OmetriaBasketItem];
+}
+
 type OmetriaReactNativeSdkType = {
-  multiply(a: number, b: number): Promise<number>;
   initializeWithApiToken(token: string): Promise<any>;
+  trackProfileIdentifiedEventByCustomerID(customerId: string): Promise<any>;
+  trackProfileIdentifiedEventByEmail(email: string): Promise<any>;
+  trackProductViewedEvent(productId: string): Promise<any>;
+  trackProductCategoryViewedEvent(category: string): Promise<any>;
+  trackWishlistAddedToEvent(productId: string): Promise<any>;
+  trackWishlistRemovedFromEvent(): Promise<any>;
+  trackBasketViewedEvent(): Promise<any>;
+  trackBasketUpdatedEvent(basket: OmetriaBasket): Promise<any>;
+  trackOrderCompletedEvent(orderId: string, basket: OmetriaBasket): Promise<any>;
+  trackDeepLinkOpenedEvent(link: string, screenName: string): Promise<any>;
+  trackScreenViewedEvent(screenName: string, additionalInfo: [string]): Promise<any>;
+  trackCustomEvent(customEventType: string, additionalInfo: [string]): Promise<any>;
+  flush(): Promise<any>;
+  clear(): Promise<any>;
+  isLoggingEnabled: Boolean;
 };
 
 const { OmetriaReactNativeSdk } = NativeModules;
