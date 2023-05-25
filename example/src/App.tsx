dd1cff87af2e61943ff7eb5cee5363f21b82ff12
Patrascu Vlad <vladytim@gmail.com>
2023-05-25 09:33:39 +0300

    Example | Fix eslint

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -18,7 +18,7 @@ import Ometria, {
   OmetriaNotificationData,
 } from 'react-native-ometria';
 
-import {version} from '../../package.json';
+import { version } from '../../package.json';
 
 const Events = {
   ENABLE_LOGGING: 'ENABLE_LOGGING',
@@ -68,7 +68,7 @@ const App = () => {
 
   const [initPN, setInitPN] = useState(false); // isReady to initialize Push Notification
   const [notificationContent, setNotificationContent] = useState(
-    'Interract with a notification to see its content here.',
+    'Interract with a notification to see its content here.'
   );
 
   const [authModal, setAuthModal] = useState(false);
@@ -96,9 +96,9 @@ const App = () => {
           await requestPNPermission();
           setInitPN(true);
         },
-        error => {
+        (error) => {
           throw error;
-        },
+        }
       );
     } catch (error) {
       console.error('😕 Error: ', error);
@@ -154,7 +154,7 @@ const App = () => {
 
     // Listen for new FCM tokens and provide them to the Ometria SDK
     messaging().onTokenRefresh((pushToken: string) =>
-      Ometria.onNewToken(pushToken),
+      Ometria.onNewToken(pushToken)
     );
 
     // Set up a listener for user interaction with push notifications
@@ -186,7 +186,7 @@ const App = () => {
       async (remoteMessage: any) => {
         console.log('📭 Foreground message received:', remoteMessage);
         Ometria.onMessageReceived(remoteMessage);
-      },
+      }
     );
 
     messaging().setBackgroundMessageHandler(async (remoteMessage: any) => {
@@ -201,17 +201,17 @@ const App = () => {
    * Handle Deeplinking
    * @param payload {url: String}
    */
-  const handleDeepLinking = ({url}: any) => {
-    Linking.canOpenURL(url).then(supported => {
+  const handleDeepLinking = ({ url }: any) => {
+    Linking.canOpenURL(url).then((supported) => {
       if (supported) {
         Ometria.processUniversalLink(url).then(
-          response => {
+          (response) => {
             Alert.alert('🔗 URL processed:', response);
           },
-          error => {
+          (error) => {
             console.log(error);
             Alert.alert('🔗 Unable to process URL: ' + url);
-          },
+          }
         );
       }
     });
@@ -221,7 +221,7 @@ const App = () => {
    * Handle Login by Email or UserId
    * @param method {userEmail?: String, customerId?: String}
    */
-  const handleLogin = (method: {userEmail?: string; userId?: string}) => {
+  const handleLogin = (method: { userEmail?: string; userId?: string }) => {
     method.userEmail &&
       Ometria.trackProfileIdentifiedByEmailEvent(method.userEmail!);
     method.userId &&
@@ -279,7 +279,7 @@ const App = () => {
 const EventsModal: React.FC<{
   isVisible: boolean;
   onClose: () => void;
-}> = ({isVisible, onClose}) => {
+}> = ({ isVisible, onClose }) => {
   const sendEvent = (eventType: string) => {
     switch (eventType) {
       case Events.ENABLE_LOGGING:
@@ -289,7 +289,7 @@ const EventsModal: React.FC<{
         Ometria.trackDeepLinkOpenedEvent('/profile', 'ProfileScreen');
         break;
       case Events.SCREEN_VIEWED:
-        Ometria.trackScreenViewedEvent('OnboardingScreen', {a: '1', b: '2'});
+        Ometria.trackScreenViewedEvent('OnboardingScreen', { a: '1', b: '2' });
         break;
       case Events.HOME_SCREEN_VIEWED:
         Ometria.trackHomeScreenViewedEvent();
@@ -349,18 +349,20 @@ const EventsModal: React.FC<{
       animationType="slide"
       transparent={true}
       visible={isVisible}
-      onRequestClose={onClose}>
+      onRequestClose={onClose}
+    >
       <View style={styles.container}>
         <Text style={styles.title}>Events 📝</Text>
         <TouchableOpacity style={styles.closeBtn} onPress={onClose}>
           <Text style={styles.text}>CLOSE EVENTS</Text>
         </TouchableOpacity>
         <ScrollView>
-          {Object.values(Events).map(eventValue => (
+          {Object.values(Events).map((eventValue) => (
             <TouchableOpacity
               key={eventValue}
               style={styles.btn}
-              onPress={() => sendEvent(eventValue)}>
+              onPress={() => sendEvent(eventValue)}
+            >
               <Text style={styles.text}>{eventValue}</Text>
             </TouchableOpacity>
           ))}
@@ -383,7 +385,8 @@ const AuthModal: React.FC<{
       animationType="slide"
       transparent={true}
       visible={isVisible}
-      onRequestClose={onClose}>
+      onRequestClose={onClose}
+    >
       <View style={styles.container}>
         <Text style={styles.title}>Change Login Info 🔐</Text>
         <TextInput
@@ -396,8 +399,9 @@ const AuthModal: React.FC<{
         <TouchableOpacity
           style={styles.btn}
           onPress={() => {
-            onLogin({userId});
-          }}>
+            onLogin({ userId });
+          }}
+        >
           <Text style={styles.text}>Login with customer ID</Text>
         </TouchableOpacity>
         <TextInput
@@ -409,7 +413,8 @@ const AuthModal: React.FC<{
         />
         <TouchableOpacity
           style={styles.btn}
-          onPress={() => onLogin({userEmail})}>
+          onPress={() => onLogin({ userEmail })}
+        >
           <Text style={styles.text}>Login with customer Email</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.closeBtn} onPress={onClose}>
