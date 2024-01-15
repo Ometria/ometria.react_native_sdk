6e9192c5b5abc99f32913dc5708814aff5f3b0aa
Patrascu Vlad <vlad.patrascu@tapptitude.com>
2024-01-15 10:56:04 +0200

    Version Bump - 2.3.1

--- android/src/main/java/com/ometriareactnativesdk/OmetriaReactNativeSdkModule.kt
+++ android/src/main/java/com/ometriareactnativesdk/OmetriaReactNativeSdkModule.kt
@@ -15,7 +15,7 @@ class OmetriaReactNativeSdkModule(private val reactContext: ReactApplicationCont
   var deeplinkInteractionPromise: Promise? = null
 
   init {
-    StorageController(reactContext.applicationContext).saveSdkVersionRN("2.3.0")
+    StorageController(reactContext.applicationContext).saveSdkVersionRN("2.3.1")
   }
 
   override fun getName(): String {
