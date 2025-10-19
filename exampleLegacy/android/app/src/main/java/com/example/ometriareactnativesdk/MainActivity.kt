ba376254667331a2784b56144d3970bd8da8157e
Vlad T <65488419+vlad-timotei@users.noreply.github.com>
2025-10-19 10:16:04 +0300

    SampleApp | Android | Rename package

--- exampleLegacy/android/app/src/main/java/com/example/ometriareactnativesdk/MainActivity.kt
+++ exampleLegacy/android/app/src/main/java/com/example/ometriareactnativesdk/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sampleapp
+package com.example.ometriareactnativesdk
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- exampleLegacy/android/app/src/main/java/com/example/ometriareactnativesdk/MainApplication.kt
+++ exampleLegacy/android/app/src/main/java/com/example/ometriareactnativesdk/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.sampleapp
+package com.example.ometriareactnativesdk
 
 import android.app.Application
 import com.facebook.react.PackageList
