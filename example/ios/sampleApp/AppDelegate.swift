b7039dd9d844456186104ab09662d4b3e3fb091c
Vlad T <65488419+vlad-timotei@users.noreply.github.com>
2025-10-19 16:02:52 +0300

    SampleApp | Restore name and version

--- example/ios/sampleApp/AppDelegate.swift
+++ example/ios/sampleApp/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "sampleApp",
+      withModuleName: "OmetriaReactNativeSdkExample",
       in: window,
       launchOptions: launchOptions
     )
